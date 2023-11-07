c6f394aa15126844227f35cbed88d28b39479449
Sara Panfini <sara.panfini@studio.unibo.it>
2023-11-07 16:50:26 +0100

    Exercise 3 done

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,9 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if(failProbability < 0 || failProbability >= 1) {
+            throw new IllegalArgumentException("Probability must be within 0 and 1, but: " + failProbability);
+        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
@@ -55,7 +58,6 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
             commandQueue.add(data);
         } else {
             final var message = data + " is not a valid keyword (allowed: " + KEYWORDS + "), nor is a number";
-            System.out.println(message);
             commandQueue.clear();
             /*
              * This method, in this point, should throw an IllegalStateException.
@@ -64,6 +66,8 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
              *
              * The previous exceptions must be set as the cause of the new exception
              */
+
+            throw new IllegalArgumentException(message, exceptionWhenParsedAsNumber);           
         }
     }
 
@@ -79,7 +83,7 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
 
     private void accessTheNetwork(final String message) throws IOException {
         if (randomGenerator.nextDouble() < failProbability) {
-            throw new IOException("Generic I/O error");
+            throw message == null ? new NetworkException() : new NetworkException(message);
         }
     }
 
