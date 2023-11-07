7af26b6b6941b6785fa40c6685393221e6e1d685
Sara Panfini <sara.panfini@studio.unibo.it>
2023-11-07 11:11:59 +0100

    import solutions

---
+++ sol/63-exceptions/build.gradle.kts
@@ -0,0 +1,15 @@
+import org.gradle.kotlin.dsl.registering
+
+plugins {
+    application
+    java
+    id("org.danilopianini.gradle-java-qa") version "1.24.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("it.unibo.exceptions.UseArithmeticService")
+}
